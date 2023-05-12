0b1790b456c65a870cf1c62fa762b19efb5f1387
renanmetrobi <renan@metrobi.com>
2023-05-12 19:18:03 -0300

    add react reanimated 3

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming, } from 'react-native-reanimated';
 
 const Home: React.FC = () => {
+    const sharedValue = useSharedValue(10)
+    console.log("🚀 ~ file: index.tsx:8 ~ sharedValue:", sharedValue)
+    const animationFont = useAnimatedStyle(() => {
+        return {
+            fontSize: withTiming(sharedValue.value)
+        }
+    }, [])
     const navigation = useNavigation()
     return (
         <View>
-            <Text onPress={() => navigation.navigate("Signin")}>Home</Text>
+            <Text onPress={() => sharedValue.value = Math.random() * 10}>Testing shared Value</Text>
+            <Animated.Text style={animationFont} onPress={() => navigation.navigate("Signin")}>Home</Animated.Text>
         </View>
     )
 }
