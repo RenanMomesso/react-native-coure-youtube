c87ee17bbb9270a169701d3dbc1838ecd933f87e
renanmetrobi <renan@metrobi.com>
2023-05-28 19:17:22 -0300

    calendar added lib

---
+++ src/components/FullScreenCalendar/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Alert, View, Dimensions } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+const { height } = Dimensions.get('window')
+const FullScreenCalendar: React.FC = ({ onPress, setBirthDate }) => {
+    return (
+        <View onTouchStart={() => onPress} style={{
+            backgroundColor: 'white', zIndex: 1000,
+            position: 'absolute',
+            right: 0, left: 0,
+            elevation: 5,
+            marginHorizontal: 8,
+            marginTop: 20,
+
+
+        }}>
+            <Calendar
+
+                onDayPress={(day) => {
+                    setBirthDate(day.dateString)
+                    onPress()
+
+                }}
+                style={{
+                    backgroundColor: "transparent",
+                    borderRadius: 10
+                }}
+            />
+        </View>
+    )
+}
+
+export default FullScreenCalendar;
--- src/pages/FillProfileScreen/index.tsx
+++ src/pages/FillProfileScreen/index.tsx
@@ -8,10 +8,13 @@ import { useDispatch } from 'react-redux';
 import { clearUserAction } from 'src/store/actions/userActions';
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Button from '@components/Button';
+import FullScreenCalendar from '@components/FullScreenCalendar';
 
 const FillProfileScreen: React.FC<any> = ({ navigation }) => {
 
     const [email, setEmail] = React.useState("")
+    const [calendarOpen, setCalendarOpen] = React.useState(false)
+    const [birthDate, setBirthDate] = React.useState("")
 
     const emailRef = useRef<any>(null);
 
@@ -25,93 +28,100 @@ const FillProfileScreen: React.FC<any> = ({ navigation }) => {
         return true;
     });
 
+    const handleLoseAllFocus = () => {
+        setCalendarOpen(false)
+    }
+
 
     return (
-        <Pressable style={{ paddingHorizontal: 20, paddingTop: 40, backgroundColor: "#FFF", flex: 1 }}>
-            <HeaderNavigation title='Fill Your Profile' navigation={navigation} onPress={handleGoBack} />
-            <ProfilePhoto />
-            <TextInputIcon
-                // isFocused={emailFocused}
-                keyboardType='email-address'
-                ref={emailRef}
-                placeholder='Email'
-                value={email}
-                onChangeText={setEmail}
-                returnKeyType='next'
-                // endEdditing={() => passwordRef.current?.focus()}
-                // onFocus={() => setEmailFocused(true)}
-                // onBlur={() => setEmailFocused(false)}
-                placeholderTextColor={"lightgray"}
-            />
-            <TextInputIcon
-                // isFocused={emailFocused}
-                keyboardType='email-address'
-                ref={emailRef}
-                placeholder='Nickname'
-                value={email}
-                onChangeText={setEmail}
-                returnKeyType='next'
-                // endEdditing={() => passwordRef.current?.focus()}
-                // onFocus={() => setEmailFocused(true)}
-                // onBlur={() => setEmailFocused(false)}
-                placeholderTextColor={"lightgray"}
-            />
-            <TextInputIcon
-                // isFocused={emailFocused}
-                keyboardType='email-address'
-                ref={emailRef}
-                placeholder='Date of Birth'
-                value={email}
-                onChangeText={setEmail}
-                returnKeyType='next'
-                // endEdditing={() => passwordRef.current?.focus()}
-                // onFocus={() => setEmailFocused(true)}
-                // onBlur={() => setEmailFocused(false)}
-                placeholderTextColor={"lightgray"}
-                rightIconName={<Icons name='calendar' size={20} color='lightgray' />}
+        <>
+            {calendarOpen && <FullScreenCalendar setBirthDate={setBirthDate} onPress={() => setCalendarOpen(false)} />}
+            <Pressable onPress={handleLoseAllFocus} style={{ paddingHorizontal: 20, paddingTop: 40, backgroundColor: "#FFF", flex: 1 }}>
+                <HeaderNavigation title='Fill Your Profile' navigation={navigation} onPress={handleGoBack} />
+                <ProfilePhoto />
+                <TextInputIcon
+                    // isFocused={emailFocused}
+                    keyboardType='email-address'
+                    ref={emailRef}
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={setEmail}
+                    returnKeyType='next'
+                    // endEdditing={() => passwordRef.current?.focus()}
+                    // onFocus={() => setEmailFocused(true)}
+                    // onBlur={() => setEmailFocused(false)}
+                    placeholderTextColor={"lightgray"}
                 />
-            <TextInputIcon
-                // isFocused={emailFocused}
-                keyboardType='email-address'
-                ref={emailRef}
-                placeholder='Email'
-                value={email}
-                onChangeText={setEmail}
-                returnKeyType='next'
-                // endEdditing={() => passwordRef.current?.focus()}
-                rightIconName={<Icons name='email' size={20} color='lightgray' />}
-                // onFocus={() => setEmailFocused(true)}
-                // onBlur={() => setEmailFocused(false)}
-                placeholderTextColor={"lightgray"}
-            />
-            <TextInputIcon
-                // isFocused={emailFocused}
-                keyboardType='email-address'
-                ref={emailRef}
-                placeholder='Phone Number'
-                value={email}
-                onChangeText={setEmail}
-                returnKeyType='next'
-                // endEdditing={() => passwordRef.current?.focus()}
-                // onFocus={() => setEmailFocused(true)}
-                // onBlur={() => setEmailFocused(false)}
-                placeholderTextColor={"lightgray"}
-            />
-            <TextInputIcon
-                // isFocused={emailFocused}
-                keyboardType='email-address'
-                ref={emailRef}
-                placeholder='Gender'
-                value={email}
-                onChangeText={setEmail}
-                returnKeyType='next'
-                // endEdditing={() => passwordRef.current?.focus()}
-                // onFocus={() => setEmailFocused(true)}
-                // onBlur={() => setEmailFocused(false)}
-                placeholderTextColor={"lightgray"}
-            />
-            <Button text='Continue' onPress={() => Alert.alert("Filled")} />
-        </Pressable>
+                <TextInputIcon
+                    // isFocused={emailFocused}
+                    keyboardType='email-address'
+                    ref={emailRef}
+                    placeholder='Nickname'
+                    value={email}
+                    onChangeText={setEmail}
+                    returnKeyType='next'
+                    // endEdditing={() => passwordRef.current?.focus()}
+                    // onFocus={() => setEmailFocused(true)}
+                    // onBlur={() => setEmailFocused(false)}
+                    placeholderTextColor={"lightgray"}
+                />
+                <TextInputIcon
+                    // isFocused={emailFocused}
+                    keyboardType='numeric'
+                    ref={emailRef}
+                    placeholder='Date of Birth'
+                    value={birthDate}
+                    onChangeText={setBirthDate}
+                    returnKeyType='next'
+                    // endEdditing={() => passwordRef.current?.focus()}
+                    // onFocus={() => setEmailFocused(true)}
+                    // onBlur={() => setEmailFocused(false)}
+                    placeholderTextColor={"lightgray"}
+                    rightIconName={<Icons name='calendar' size={20} color='lightgray' onPress={() => setCalendarOpen(true)} />}
+                />
+                <TextInputIcon
+                    // isFocused={emailFocused}
+                    keyboardType='email-address'
+                    ref={emailRef}
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={setEmail}
+                    returnKeyType='next'
+                    // endEdditing={() => passwordRef.current?.focus()}
+                    rightIconName={<Icons name='email' size={20} color='lightgray' />}
+                    // onFocus={() => setEmailFocused(true)}
+                    // onBlur={() => setEmailFocused(false)}
+                    placeholderTextColor={"lightgray"}
+                />
+                <TextInputIcon
+                    // isFocused={emailFocused}
+                    keyboardType='email-address'
+                    ref={emailRef}
+                    placeholder='Phone Number'
+                    value={email}
+                    onChangeText={setEmail}
+                    returnKeyType='next'
+                    // endEdditing={() => passwordRef.current?.focus()}
+                    // onFocus={() => setEmailFocused(true)}
+                    // onBlur={() => setEmailFocused(false)}
+                    placeholderTextColor={"lightgray"}
+                />
+                <TextInputIcon
+                    // isFocused={emailFocused}
+                    keyboardType='email-address'
+                    ref={emailRef}
+                    placeholder='Gender'
+                    value={email}
+                    onChangeText={setEmail}
+                    returnKeyType='next'
+                    // endEdditing={() => passwordRef.current?.focus()}
+                    // onFocus={() => setEmailFocused(true)}
+                    // onBlur={() => setEmailFocused(false)}
+                    placeholderTextColor={"lightgray"}
+                />
+                <Button text='Continue' onPress={() => Alert.alert("Filled")} />
+            </Pressable>
+        </>
 
     )
 }
